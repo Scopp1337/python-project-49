b0afd28c0fe5e8af0e65d03df2a8eb735f830a07
Roman <roma.mazin.47@gmail.com>
2025-10-06 20:46:21 +0300

    Add dev ruff in pypproject.toml and add ruff.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
-from brain_games.cli import welcome_user
+from brain_games.scripts.brain_games.py import welcome_user
+
 
 def greet():
     print('Welcome to Brain Games!')
 
+
 def main():
     greet()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
