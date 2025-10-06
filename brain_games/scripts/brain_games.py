c0cad657d803bd1fc43622d508cbf7e65bd2395b
Roman <roma.mazin.47@gmail.com>
2025-10-06 19:09:07 +0300

    Add func welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
+from brain_games.cli import welcome_user
+
 def greet():
     print('Welcome to Brain Games!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
